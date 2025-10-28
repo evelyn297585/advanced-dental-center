aa4e83a474f878b738dac2ddb30525c4ee48b0b9
evelyn297585 <evelyn.quirozl@outlook.com>
2025-10-27 18:50:13 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,10 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
